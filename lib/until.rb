0582c020846c619bfcc2bef3e373a27a6c73a6f6
Stevi Hall <career.sh.work@gmail.com>
2020-03-07 00:10:34 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
-def using_until
-end
-  levitation_force = 6
-until levitation_force == 10
+#def using_until
+#end
+ # levitation_force = 6
+#until levitation_force == 10
 puts "Wingardium Leviosa"  
 levitation_force += 1
 end
